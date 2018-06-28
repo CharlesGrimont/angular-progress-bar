6a135d98decd236d26379116fa190437813dfc49
Grimont <grimont.c@gmail.com>
2018-06-28 19:40:05 +0200

    #2 Release new beta @1.0.1-beta4

--- src/progress-bar/progress-bar.ts
+++ src/progress-bar/progress-bar.ts
@@ -46,6 +46,7 @@ export class ProgressBarComponent {
 
 
   constructor() {
+    // Default color
     this.color = "#488aff";
   }
 
@@ -54,16 +55,28 @@ export class ProgressBarComponent {
    * @param percent The current progress
    */
   whichColor(percent: string){
-    let keys = Object.keys(this.degraded).sort();
-    let last = keys[0];
-    for(let val of keys){
-      if(val < percent){
+    // Get all entries index as an array
+    let k: Array<any> = Object.keys(this.degraded);
+    // Convert string to number
+    k.forEach((e, i) => k[i] = +e);
+    // Sort them by value
+    k = k.sort((a, b) => a - b);
+    // Percent as number
+    let p = +percent
+    // Set last by default as the first occurence
+    let last = k[0];
+    // Foreach keys 
+    for(let val of k){
+      // if current val is < than percent
+      if(val < p){
         last = val;
       }
-      else if(val > percent){
+      // if val >= percent then the val that we could show has been reached
+      else if(val >= p -1){
         return this.degraded[last];
       }
     }
+    // if its the last one retrun the last
     return this.degraded[last];
   }
 }
