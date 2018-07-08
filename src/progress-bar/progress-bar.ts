c6c80eb5dd564455de8dfd0141286ac2c7fc18e7
Grimont <grimont.c@gmail.com>
2018-07-08 16:54:58 +0200

    revert to older format and stabilize use into angular

--- src/progress-bar/progress-bar.ts
+++ src/progress-bar/progress-bar.ts
@@ -30,7 +30,7 @@ import { Component, Input  } from '@angular/core';
     `],
     template:
         "<div class=\"progress-outer\">\n" +
-        "    <div class=\"progress-inner\" [style.width]=\"progress + '%'\" [style.background-color]=\"color != null ? color : '#488aff'\">\n" +
+        "    <div class=\"progress-inner\" [style.width]=\"progress + '%'\" [style.background-color]=\"degraded == null ? color : whichColor(progress)\">\n" +
         "        {{progress}}%\n" +
         "</div>\n" +
         "</div>"
@@ -42,8 +42,41 @@ export class ProgressBarComponent {
   /** Inputs **/
     @Input('progress') progress: string;
     @Input('color') color: string;
+    @Input('color-degraded') degraded: any;
+
 
   constructor() {
+    // Default color
+    this.color = "#488aff";
+  }
 
+  /**
+   * Returns a color for a certains percent
+   * @param percent The current progress
+   */
+  whichColor(percent: string){
+    // Get all entries index as an array
+    let k: Array<any> = Object.keys(this.degraded);
+    // Convert string to number
+    k.forEach((e, i) => k[i] = +e);
+    // Sort them by value
+    k = k.sort((a, b) => a - b);
+    // Percent as number
+    let p = +percent
+    // Set last by default as the first occurence
+    let last = k[0];
+    // Foreach keys 
+    for(let val of k){
+      // if current val is < than percent
+      if(val < p){
+        last = val;
+      }
+      // if val >= percent then the val that we could show has been reached
+      else if(val >= p -1){
+        return this.degraded[last];
+      }
+    }
+    // if its the last one retrun the last
+    return this.degraded[last];
   }
 }
